23ed58cc0c2910d2e15d30d6b95aca614c773edc
Илья Воробьёв <iliya.vor2021@yandex.ru>
2023-04-25 01:18:46 +0300

    bug done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://158.160.33.43/']
+
 DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='127.0.0.1')]
