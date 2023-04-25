9aea3e3248eb7d803c0c80312910bf8013b259df
Илья Воробьёв <iliya.vor2021@yandex.ru>
2023-04-25 20:43:02 +0300

    bug done

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,8 @@
+from django_filters import ModelMultipleChoiceFilter
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 from users.models import User
 
 
@@ -14,14 +15,18 @@ class RecipeFilter(FilterSet):
     """
     Доступна фильтрация по избранному, автору, списку покупок и тегам.
     """
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = filters.BooleanFilter(method='filter_favorited')
     is_in_shopping_cart = filters.BooleanFilter(method='filter_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('tags', 'author')
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
